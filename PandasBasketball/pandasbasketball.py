db741a9c5cef92fa8ce7a4792d9ec4ade8bf8d8a
Alfredo AMEP2. Medina Palacios <alfredo.medina@stkrursus.com>
2019-01-29 11:47:12 -0600

    Add functionality and improve readability
    Method 'get_player_season' is now available. Also, cleaner code due to
    the 'get_data' function.

--- PandasBasketball/pandasbasketball.py
+++ PandasBasketball/pandasbasketball.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-from PandasBasketball.stats import player_stats, team_stats
+from PandasBasketball.stats import player_stats, team_stats, player_season
 from PandasBasketball.errors import StatusCode404
 
 BASE_URL = "https://www.basketball-reference.com"
 
-def get_player(code, stat, numeric=False, s_index=False):
+def get_player(player, stat, numeric=False, s_index=False):
     """
     Returns a pandas dataframe with the player's stats
     \tKeyword arguments:
@@ -18,7 +18,7 @@ def get_player(code, stat, numeric=False, s_index=False):
     """
 
     # Building the url and making the request
-    url = BASE_URL + f"/players/{code[0]}/{code}.html"
+    url = BASE_URL + f"/players/{player[0]}/{player}.html"
     r = requests.get(url)
 
     # If the page is not found, raise the error
@@ -27,6 +27,20 @@ def get_player(code, stat, numeric=False, s_index=False):
         raise StatusCode404
     else:
         return player_stats(r, stat, numeric=numeric, s_index=s_index)
+
+def get_player_season(player, season):
+    """
+    """
+
+    url = BASE_URL + f"/players/{player[0]}/{player}/gamelog/{season}"
+    r = requests.get(url)
+
+    if r.status_code == 404:
+        raise StatusCode404
+    else:
+        return player_season(r)
+
+
 
 def get_team(team):
     """
--- PandasBasketball/stats.py
+++ PandasBasketball/stats.py
@@ -9,11 +9,6 @@ def player_stats(request, stat, numeric=False, s_index=False):
                         "playoffs_per_game", "playoffs_totals", "playoffs_per_minute",
                         "playoffs_per_poss", "playoffs_advanced"]
 
-    columns = []
-    seasons = []
-    stats = []
-    data = []
-
     if stat == "per_game":
         soup = BeautifulSoup(request.text, "html.parser")
         table = soup.find("table", id="per_game")
@@ -25,38 +20,9 @@ def player_stats(request, stat, numeric=False, s_index=False):
     else:
         raise TableNonExistent
 
-    rows = table.find_all("tr")
-    heading = table.find("thead")
-    heading_row = heading.find("tr")
+    # Get the whole data frame
+    df = get_data(table)
 
-    # Getting the column names
-    for x in heading_row.find_all("th"):
-        columns.append(x.string)
-    # Gettin the 'season' column values
-    for row in rows:
-        a = row.find_all("a")
-        for season in a:
-            if season.string[0] == "1" or season.string[0] == "2":
-                seasons.append(season.string)
-            else: 
-                continue
-    # Getting the actual table values 
-    for row in rows:
-        line = row.find_all("td")   
-        for value in line:
-            stats.append(value.string)
-    # Joining all data into 'data'
-    for i in range(len(seasons)):
-        data.append(seasons[i])
-        for j in range(len(columns) - 1):
-            data.append(stats[j])
-        del(stats[:len(columns) - 1])
-    # Making a list of tuples
-    data = list(zip(*[iter(data)]*len(columns)))
-    # Builing the data frame
-    df = pd.DataFrame(data)
-    df.columns = columns
-    # Delete extra columns
     if stat == "per_poss" or stat == "playoffs_per_poss":
         del df[None]
     elif stat == "advanced" or stat == "playoffs_advanced":
@@ -67,25 +33,42 @@ def player_stats(request, stat, numeric=False, s_index=False):
     if s_index:
         df.set_index("Season", inplace=True)
 
+    return df
+
+def player_season(request):
+
+    soup = BeautifulSoup(request.text, "html.parser")
+    table = soup.find("table", class_="row_summable sortable stats_table")
+
+    df = get_data(table)
+    del df["Rk"]
 
     return df
 
+
 def team_stats(request, team):
     
+    soup = BeautifulSoup(request.text, "html.parser")
+    table = soup.find("table", id=team)
+
+    df = get_data(table)
+    del df["\xa0"]
+
+    return df
+
+def get_data(table):
+
     columns = []
     seasons = []
     stats = []
     data = []
-
-    soup = BeautifulSoup(request.text, "html.parser")
-    table = soup.find("table", id=team)
 
     rows = table.find_all("tr")
     heading = table.find("thead")
     heading_row = heading.find("tr")
 
     for x in heading_row.find_all("th"):
-        columns.append(x.string)
+            columns.append(x.string)
     for row in rows:
         a = row.find_all("a")
         for season in a:
@@ -106,6 +89,5 @@ def team_stats(request, team):
     data = list(zip(*[iter(data)]*len(columns)))
     df = pd.DataFrame(data)
     df.columns = columns
-    del df["\xa0"]
 
     return df
