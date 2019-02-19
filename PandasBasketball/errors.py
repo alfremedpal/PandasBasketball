6f35f1ba502fb91ebf962a211e517a3cd4e49635
Alfredo AMEP2. Medina Palacios <alfredo.medina@stkrursus.com>
2019-02-19 16:56:53 -0600

    Update error description message

--- PandasBasketball/errors.py
+++ PandasBasketball/errors.py
@@ -1,13 +1,18 @@
 
 class StatusCode404(Exception):
     def __init__(self):
-        message = """The page could not be found; url build failed.\n
-                    You could have mistyped the player or the team"""
+        message = """
+                    The page could not be found; url build failed.
+                    You could have mistyped the player code or the team.
+                    """
         super().__init__(message)
 
 class TableNonExistent(Exception):
     def __init__(self):
-        message = """The stat table could not be found.\n
-                    You could have mistyped the table name.\n
-                    If it was a playoffs table the player might not have partipated in playoffs yet."""
+        message = """
+                    The table could not be found.
+                    You could have mistyped the table name.
+                    If it was a playoffs table the player might not have partipated in playoffs yet.
+                    If it was for the last 'n' days, it is possible there is no info for that day range.
+                    """
         super().__init__(message)
